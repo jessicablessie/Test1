7dd85515484e0efe514a697ada517c08690c0243
Jessica Paga <jessica.rayray@hotmail.com>
2021-03-08 22:31:57 +1300

    Initial version

---
+++ Test1/Controllers/PatientController.cs
@@ -0,0 +1,82 @@
+﻿
+using System.Web.Mvc;
+using Test1.Models;
+//using System.Data.SqlClient;
+//using System.Configuration;
+//using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+
+namespace Test1.Controllers
+{
+    public class PatientController : Controller
+    {
+
+
+		////This returns the connection string  
+		//private static string _connectionString = string.Empty;
+
+		//public static string ConnectionString
+		//{
+		//	get
+		//	{
+		//		if (_connectionString == string.Empty)
+		//		{
+		//			_connectionString = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
+		//		}
+		//		return _connectionString;
+		//	}
+		//}
+
+		//private SqlConnection conn;
+		//private void connection()
+		//{
+		//	conn = new SqlConnection(ConnectionString);
+
+		//}
+
+		public ActionResult AddNewPatient(PatientModel pat)
+		{
+			if (ModelState.IsValid)
+			{
+
+				try
+				{
+					string JsonResult = JsonConvert.SerializeObject(pat);
+					string path = Server.MapPath("~/App_Data/patient.json");
+
+					using (var tw = new StreamWriter(path, true))
+					{
+						tw.WriteLine(JsonResult.ToString());
+						tw.Close();
+					}
+				}
+				catch { }
+
+
+				//connection();
+				//SqlCommand cmd = new SqlCommand("InsertPatient", conn);
+				//cmd.CommandType = CommandType.StoredProcedure;
+				//cmd.Parameters.AddWithValue("@FirstName",pat.FirstName);
+				//cmd.Parameters.AddWithValue("@LastName", pat.LastName);
+				//conn.Open();
+
+				//int i = cmd.ExecuteNonQuery();
+				//conn.Close();
+				//if (i >= 1)
+				//{
+
+				//	ViewBag.Message = "Record has been added successfully.";
+
+				//}
+
+				//GenerateJson(pat);
+			}
+
+			ModelState.Clear();
+			return View();
+		}
+
+	}
+}
---
+++ Test1/Models/PatientModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Test1.Models
+{
+	public class PatientModel
+	{
+		[Display(Name = "First Name")]
+		[Required(ErrorMessage = "First Name is required.")]
+		public string FirstName
+		{
+			get;
+			set;
+		}
+
+		[Display(Name = "Last Name")]
+		[Required(ErrorMessage = "Last Name is required.")]
+		public string LastName
+		{
+			get;
+			set;
+		}
+
+	}
+
+}
